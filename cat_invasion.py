894d77503fa88796639e9b957f0774663360eb6d
DanyFed1 <danyfed.study@gmail.com>
2023-09-28 08:30:41 +0200

    Small updates to the cat_ivasion.py: Color+locking frame rate

--- cat_invasion.py
+++ cat_invasion.py
@@ -8,8 +8,13 @@ class CatInvasion:
         """Overall class to manage game assets and behavior."""
         pygame.init()
         
+        self.clock = pygame.time.Clock() #We measure the time it takes to iterate over the
+        
         self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Cat Invasion")
+        
+        # Set the background color
+        self.bg_color = (230, 230, 230)
         
     def run_game(self):
         """Start the main loop for the game."""
@@ -18,11 +23,17 @@ class CatInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
             
             #Make the most recently drawn screen visible.
             pygame.display.flip()
             
+            #controlling the frame rate based on the clocl measurement. Every time we run faster
+            #refresh rate should be stable.
+            self.clock.tick(60) #set the frame rate to 60
+            
 if __name__ == "__main__":
     #Make a game instance, and run the game,
-    ai = AlienInvasion()
+    ai = CatInvasion()
     ai.run_game()
